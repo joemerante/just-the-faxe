b1f07053ad369d220c2f76605ee4cf49faa74a7c
Joe Merante <merante.joe@gmail.com>
2019-06-09 13:05:20 -0400

    simple webpack default to dist/main.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-// https://github.com/techfort/LokiJS/wiki
+import _ from 'lodash';
 
-var loki = require('lokijs')
-var guitars = new loki('Guitars')
-
-var makers = guitars.addCollection('makers', { indices: ['name'] });
-undefined
-var gibson = makers.insert( {name: 'Gibson', location: 'Nashville'} )
-
-let g = makers.findOne('name', 'Gibson')
-
-console.log(g)
-
-let customFilter = (obj) => {
-  return obj.name.match(/regex/)
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
 }
+
+document.body.appendChild(component());
