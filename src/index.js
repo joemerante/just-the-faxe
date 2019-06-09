ffb094ed07f912766800453d831ae5d2a54ccb85
Joe Merante <merante.joe@gmail.com>
2019-06-09 14:44:15 -0400

    Just the fAxe

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import _ from 'lodash';
+import loki from 'lokijs';
 
-function component() {
+let db = new loki('Guitars');
+let makers = db.addCollection('makers', { indices: ['name'] });
+
+// TODO - move a few of thse to seeds.js
+let gibson = makers.insert( {name: 'Gibson', location: 'Nashville'} )
+let fender = makers.insert( {name: 'Fender', location: 'Corona'} )
+
+// move these somewhere on the tableComponent
+// let g = makers.findOne('name', 'Gibson')
+// let customFilter = (obj) => {
+//   return obj.name.match(/regex/)
+// }
+
+function tableComponent() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const list = document.createElement('ul');
+  const makers = db.getCollection('makers');
+
+  makers.data.forEach(obj => {
+    let li = document.createElement('li');
+    li.innerHTML = obj.name;
+    list.appendChild(li);
+  });
+
+  element.appendChild(list);
+
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(tableComponent(makers));
